d68fd6cd86a4f162a548e314dec4f3a173204dce
commando001 <mkgideon@live.co.uk>
2014-08-23 09:31:49 +0100

    Student Column Attribute: FirstName

--- CollegeManagementSystem/CollegeManagementSystem/Models/Student.cs
+++ CollegeManagementSystem/CollegeManagementSystem/Models/Student.cs
@@ -4,14 +4,21 @@ using System.Collections.Generic;
 //using System.Web;
 
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CollegeManagementSystem.Models
 {
     public class Student
     {
         public int ID { get; set; } //primary key
+
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [Column("FirstName")]
+        [StringLength(50, ErrorMessage="First Name cannot be longer than 50 characters.")]
         public string FirstMidName { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
         public DateTime EnrollmentDate { get; set; }
